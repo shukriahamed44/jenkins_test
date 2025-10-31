58a1e342621afabff1d2ee40392783df0233908b
J S Ahamed <115306170+shukriahamed44@users.noreply.github.com>
2025-10-31 15:01:19 +0000

    grrovy update

---
+++ pipeline.groovy
@@ -0,0 +1,17 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Print Hello World') {
+            steps {
+                echo 'Hello, World!'
+            }
+        }
+        stage('Run hello_world.py') {
+            steps {
+            checkout scm
+            sh 'python3 hello_world.py'
+            }
+        }
+    }
+}
